b875e2706176934279e3b1306de778fd5c43ad66
Jacob <jacobmarkmanmusic@gmail.com>
2021-06-20 17:30:06 +0300

    d

--- client/src/App.js
+++ client/src/App.js
@@ -86,27 +86,27 @@
 // export default App;
 
 
-import Topbar from "./components/topbar/Topbar";
-import Home from "./components/Home/Home";
-import Projects from "./components/projects/Projects";
-import Skills from "./components/skills/Skills";
-import Contact from "./components/contact/Contact";
-import "./app.scss"
-import { useState } from "react"
-import Menu from "./components/menu/Menu"
+// import Topbar from "./components/topbar/Topbar";
+// import Home from "./components/Home/Home";
+// import Projects from "./components/projects/Projects";
+// import Skills from "./components/skills/Skills";
+// import Contact from "./components/contact/Contact";
+// import "./app.scss"
+import { useState, React } from "react"
+// import Menu from "./components/menu/Menu"
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  // const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className="app">
-     <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+    <div className="app">mmm
+     {/* <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
      <div className="sections">
        <Home/>
        <Skills/>
        <Projects/>
        <Contact/>
-     </div>
+     </div> */}
     </div>
   );
 }
