b7490be43ede6d3383182f68eca1a18aae61334f
Jacob <jacobmarkmanmusic@gmail.com>
2021-06-21 21:26:42 +0300

    f

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,38 +1,38 @@
 import "./home.scss";
 import React from "react";
-// import {init} from "ityped";
-// import { useEffect, useRef} from "react";
+import {init} from "ityped";
+import { useEffect, useRef} from "react";
 
 export default function Home() {
-    // const textRef = useRef()
+    const textRef = useRef()
 
-    // useEffect(() => {
-    //     init(textRef.current, {
-    //         showCursor: true,
-    //         backDelay: 1500,
-    //         strings: ["Full Stack Developer"]
-    //     });
-    // }, []);
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            strings: ["Full Stack Developer"]
+        });
+    }, []);
 
-    // return (
-    //     <div className="home" id="home">
-    //         <div className="left">
-    //             <div className="imgContainer">
-    //                 <img src="assets/laptop.svg" alt="" />
-    //             </div>
-    //         </div>
-    //         <div className="right">
-    //             <div className="wrapper">
-    //                 <h2>Hi There, I'm</h2>
-    //                 <h1>Jacob Markman</h1>
-    //                 <h3><span ref={textRef}></span></h3>
-    //             </div>
-    //             <a href="#skills">
-    //                 <img src="assets/down.png" alt="" />
-    //             </a>
-    //         </div>
-    //     </div>
-    // )
+    return (
+        <div className="home" id="home">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/laptop.svg" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Jacob Markman</h1>
+                    <h3><span ref={textRef}></span></h3>
+                </div>
+                <a href="#skills">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
+        </div>
+    )
 
     return (
         <div>Heyyyy</div>
