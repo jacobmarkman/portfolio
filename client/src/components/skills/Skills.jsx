ba99f1d0e5a0b55389474619012a4df9435e96c0
Jacob <jacobmarkmanmusic@gmail.com>
2021-06-26 20:12:05 +0300

    version to deploy

--- client/src/components/skills/Skills.jsx
+++ client/src/components/skills/Skills.jsx
@@ -1,19 +1,30 @@
 import "./skills.scss";
+import { useState } from "react"
+
+
 
 export default function Skills() {
+
+    const [skill, setSkill] = useState("");
+
+
+
+
+
     return (
         <div className="skills" id="skills">
             <h1>Skills</h1>
             <div className="skill-icons">
-                <img src="assets/js.svg" alt="" />
-                <img src="assets/react.svg" alt="" />
-                <img src="assets/redux.svg" alt="" />
-                <img src="assets/css.svg" alt="" />
+                <img src="assets/js.svg" alt="" onMouseEnter={() => setSkill("Java Script")} onMouseLeave={() => setSkill("")}/>
+                <img src="assets/react.svg" alt="" onMouseEnter={() => setSkill("React")} onMouseLeave={() => setSkill("")}/>
+                <img src="assets/redux.svg" alt="" onMouseEnter={() => setSkill("Redux")} onMouseLeave={() => setSkill("")}/>
+                <img src="assets/css.svg" alt="" onMouseEnter={() => setSkill("CSS")} onMouseLeave={() => setSkill("")}/>
                 {/* <img src="assets/styled-components.png" alt="" /> */}
-                <img src="assets/node-js.svg" alt="" />
-                <img src="assets/git.svg" alt="" />
-                <img src="assets/ts.svg" alt="" />
+                <img src="assets/node-js.svg" alt="" onMouseEnter={() => setSkill("Node.js")} onMouseLeave={() => setSkill("")}/>
+                <img src="assets/git.svg" alt="" onMouseEnter={() => setSkill("Git")} onMouseLeave={() => setSkill("")}/>
+                <img src="assets/ts.svg" alt="" onMouseEnter={() => setSkill("Type Script")} onMouseLeave={() => setSkill("")}/>
             </div>
+            <p>{skill}</p>
         </div>
     )
 }
