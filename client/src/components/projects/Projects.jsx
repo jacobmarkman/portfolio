e611780c25441052f765643b94fc80ef646f431b
Jacob <jacobmarkmanmusic@gmail.com>
2021-07-12 17:35:59 +0300

    f

--- client/src/components/projects/Projects.jsx
+++ client/src/components/projects/Projects.jsx
@@ -12,14 +12,15 @@ export default function Projects() {
                 <div className="item">
                     <a href="https://jacob-markman-tic-tac-toe.netlify.app" target="_blank" and rel="noopener noreferrer">
                         <img src="./assets/tictactoe.png" alt="" />
-                        <h3>Tic Tac Toe Game</h3>
                     </a>
+                    <h3>Tic Tac Toe Game</h3>
                 </div>
                 <div className="item">
-                    <a href="https://commercejs-my-store-project.netlify.app/" target="_blank" and rel="noopener noreferrer">
-                        <img src="./assets/commerce-project.png" alt="" />
-                    </a>
-                    <h3>E-Commerce Shop (in Progress)</h3>
+                    <h3>E-Commerce Shop (in Progress)
+                        <a href="https://commercejs-my-store-project.netlify.app/" target="_blank" and rel="noopener noreferrer">
+                            <img src="./assets/commerce-project.png" alt="" />
+                        </a>
+                    </h3>
                 </div>
                 <div className="item noitem">
                     <h3>In Progress...</h3>
