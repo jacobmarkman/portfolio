006f10a0f666ca77b0a44de84d661913d2159d01
Jacob <jacobmarkmanmusic@gmail.com>
2021-08-24 02:58:14 +0300

    better responsive

--- client/src/components/projects/Projects.jsx
+++ client/src/components/projects/Projects.jsx
@@ -10,44 +10,50 @@ export default function Projects() {
         <div className="projects" id="projects">
             <h1>Projects</h1>
             <div className="container">
-                <a href="https://github.com/jacobmarkman/tic-tac-toe" target="_blank">
-                    <GitHubIcon style={{ color: 'black' }} />
-                </a>
-                <div className="item">
-                    <h3>Simple Tic-Tac-Toe Game
-                        <br /><br />
-                        <h5>(HTML, CSS, JavaScript)</h5>
-                    </h3>
-                    <a href="https://jacob-markman-tic-tac-toe.netlify.app" target="_blank" and rel="noopener noreferrer">
-                        <img src="./assets/tictactoe.png" alt="" />
+                <div className="img-size">
+                    <a href="https://github.com/jacobmarkman/tic-tac-toe" target="_blank">
+                        <GitHubIcon style={{ color: 'black' }} />
                     </a>
+                    <div className="item">
+                        <h3>Simple Tic-Tac-Toe Game
+                            <br /><br />
+                            <h5>(HTML, CSS, JavaScript)</h5>
+                        </h3>
+                        <a href="https://jacob-markman-tic-tac-toe.netlify.app" target="_blank" and rel="noopener noreferrer">
+                            <img src="./assets/tictactoe.png" alt="" />
+                        </a>
+                    </div>
                 </div>
-                <div className="github"></div>
-                <a href="https://github.com/jacobmarkman/e_commerce" target="_blank">
-                <GitHubIcon style={{ color: 'black' }} />
-                </a>
+                <div className="img-size">
+                    <a href="https://github.com/jacobmarkman/e_commerce" target="_blank">
+                        <GitHubIcon style={{ color: 'black' }} />
+                    </a>
 
-                <div className="item">
-                    <h3>E-Commerce Music DAW Shop <br /><br />
-                        <h5>(React, Commerce.js, Material UI)</h5>
-                    </h3>
-                    <a href="https://commercejs-my-store-project.netlify.app/" target="_blank" and rel="noopener noreferrer">
-                        <img src="./assets/commerce-project.png" alt="" />
-                    </a>
+                    <div className="item">
+                        <h3>E-Commerce Music DAW Shop <br /><br />
+                            <h5>(React, Commerce.js, Material UI)</h5>
+                        </h3>
+                        <a href="https://commercejs-my-store-project.netlify.app/" target="_blank" and rel="noopener noreferrer">
+                            <img src="./assets/commerce-project.png" alt="" />
+                        </a>
+                    </div>
                 </div>
-                <a href="https://github.com/jacobmarkman/memories_project" target="_blank">
-                <GitHubIcon style={{ color: 'black' }} />
-                </a>
+                <div className="img-size">
+                    <a href="https://github.com/jacobmarkman/memories_project" target="_blank">
+                        <GitHubIcon style={{ color: 'black' }} />
+                    </a>
 
-                <div className="item noitem">
-                    <h3>Memories Social Media App
-                        <br /><br />
-                        <h5>(React, Redux, Node/Express/Mongoose, Material UI, MongoDB)</h5>
-                    </h3>
-                    <a href="https://memories-app-jacob-markman.netlify.app/" target="_blank" and rel="noopener noreferrer">
-                        <img src="./assets/memories.png" alt="" />
-                    </a>
+                    <div className="item noitem">
+                        <h3>Memories Social Media App
+                            <br /><br />
+                            <h5>(React, Redux, Node/Express/Mongoose, Material UI, MongoDB)</h5>
+                        </h3>
+                        <a href="https://memories-app-jacob-markman.netlify.app/" target="_blank" and rel="noopener noreferrer">
+                            <img src="./assets/memories.png" alt="" />
+                        </a>
+                    </div>
                 </div>
+
             </div>
         </div>
     )
